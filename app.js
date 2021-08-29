bb036bd9e971b90352b2377dcd12ae78a929ef73
Tautvydas <tautvydaszuk@gmail.com>
2021-08-29 22:14:50 +0300

    add play feature that alternates between X and O

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-const boardSize = 9
-const boardContainer = document.querySelector("#boardContainer")
-for (let i = 0; i < boardSize; i++) {
-    const boardTile = document.createElement("div")
-    boardTile.classList.add("boardTile")
-    boardTile.setAttribute("id", i)
-    boardContainer.appendChild(boardTile);
-}
-
-const gameBoard = (() => {
-    const gameArray = []
-    return {gameArray}
-})();
-
 const Player = (name, marker) => {
     return {name, marker};
 };
@@ -19,9 +5,32 @@ const Player = (name, marker) => {
 const player1 = Player("First", "X");
 const player2 = Player("Second", "O");
 
-const displayController = (() => {
-    for (let i = 0; i < gameBoard.gameArray.length; i++) {
-        document.querySelectorAll(".boardTile")[i].textContent = gameBoard.gameArray[i]
-    }
+const gameBoard = (() => {
+    const gameArray = ["","","","","","","","",""]
+    return {gameArray}
 })();
 
+const displayController = (() => {
+    const boardSize = 9
+    let turn = "first"
+    const boardContainer = document.querySelector("#boardContainer")
+    for (let i = 0; i < boardSize; i++) {
+        const boardTile = document.createElement("div")
+        boardTile.classList.add("boardTile")
+        boardTile.setAttribute("id", i)
+        boardContainer.appendChild(boardTile);
+    }
+    document.querySelectorAll(".boardTile").forEach(tile => {
+        tile.addEventListener("click", () => {
+            if (turn == "first" && tile.textContent==""){
+            gameBoard.gameArray.splice(tile.id, 1, player1.marker)
+            document.querySelectorAll(".boardTile")[tile.id].textContent = gameBoard.gameArray[tile.id]
+            turn = "second";
+            } else if (turn == "second" && tile.textContent=="") {
+            gameBoard.gameArray.splice(tile.id, 1, player2.marker)
+            document.querySelectorAll(".boardTile")[tile.id].textContent = gameBoard.gameArray[tile.id]
+            turn = "first";
+            }
+        })
+    })
+})();
