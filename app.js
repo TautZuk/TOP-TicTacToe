3f759b94b9b4fc3eedae08b376798f1f6267bfc8
Tautvydas <tautvydaszuk@gmail.com>
2021-08-29 20:30:01 +0300

    create and display board

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const boardSize = 9
+const boardContainer = document.querySelector("#boardContainer")
+for (let i = 0; i < boardSize; i++) {
+    const boardTile = document.createElement("div")
+    boardTile.classList.add("boardTile")
+    boardTile.setAttribute("id", i)
+    boardContainer.appendChild(boardTile);
+}
+
 const gameBoard = (() => {
-    const gameArray = ["O","O","X","X","O","O","X","O","X"]
+    const gameArray = []
     return {gameArray}
-}) ();
+})();
 
 const Player = (name, marker) => {
     return {name, marker};
@@ -9,4 +18,10 @@ const Player = (name, marker) => {
 
 const player1 = Player("First", "X");
 const player2 = Player("Second", "O");
+
+const displayController = (() => {
+    for (let i = 0; i < gameBoard.gameArray.length; i++) {
+        document.querySelectorAll(".boardTile")[i].textContent = gameBoard.gameArray[i]
+    }
+})();
 
