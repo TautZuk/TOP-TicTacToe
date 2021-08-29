9f86441df14c3af379f4d28a674c2f052eab7a4d
Tautvydas <tautvydaszuk@gmail.com>
2021-08-30 00:45:06 +0300

    add check for winner

--- app.js
+++ app.js
@@ -23,14 +23,83 @@ const displayController = (() => {
     document.querySelectorAll(".boardTile").forEach(tile => {
         tile.addEventListener("click", () => {
             if (turn == "first" && tile.textContent==""){
-            gameBoard.gameArray.splice(tile.id, 1, player1.marker)
-            document.querySelectorAll(".boardTile")[tile.id].textContent = gameBoard.gameArray[tile.id]
+            gameBoard.gameArray.splice(tile.id, 1, 1)
+            document.querySelectorAll(".boardTile")[tile.id].textContent = player1.marker
             turn = "second";
+            whoIsWinner();
             } else if (turn == "second" && tile.textContent=="") {
-            gameBoard.gameArray.splice(tile.id, 1, player2.marker)
-            document.querySelectorAll(".boardTile")[tile.id].textContent = gameBoard.gameArray[tile.id]
+            gameBoard.gameArray.splice(tile.id, 1, -1)
+            document.querySelectorAll(".boardTile")[tile.id].textContent = player2.marker
             turn = "first";
+            whoIsWinner();
             }
         })
     })
 })();
+
+const whoIsWinner = (() => {
+    switch(gameBoard.gameArray[0] + gameBoard.gameArray[1] + gameBoard.gameArray[2]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+    switch(gameBoard.gameArray[3] + gameBoard.gameArray[4] + gameBoard.gameArray[5]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+    switch(gameBoard.gameArray[6] + gameBoard.gameArray[7] + gameBoard.gameArray[8]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+    switch(gameBoard.gameArray[0] + gameBoard.gameArray[3] + gameBoard.gameArray[6]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+    switch(gameBoard.gameArray[1] + gameBoard.gameArray[4] + gameBoard.gameArray[7]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+    switch(gameBoard.gameArray[3] + gameBoard.gameArray[5] + gameBoard.gameArray[8]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+    switch(gameBoard.gameArray[0] + gameBoard.gameArray[4] + gameBoard.gameArray[8]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+    switch(gameBoard.gameArray[2] + gameBoard.gameArray[4] + gameBoard.gameArray[6]) {
+        case 3:
+            winner = player1.name
+            break;
+        case -3:
+            winner = player2.name
+            break;
+    }
+});
